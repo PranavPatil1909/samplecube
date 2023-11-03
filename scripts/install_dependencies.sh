33a89eeed3e9e523d1d374ca3ed7890ae7a3da5f
PranavPatil1909 <109128442+PranavPatil1909@users.noreply.github.com>
2023-11-03 17:18:54 +0530

    changes in script files

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -7,6 +7,7 @@
 # here we update the server and install node and npm
 echo installing dependencies
 sudo apt-get update
+sudo apt-get install apache2 -y
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install nodejs -y
 sudo apt-get -y install npm
--- scripts/start_server1.sh
+++ scripts/start_server1.sh
@@ -4,11 +4,9 @@ echo starting server
 # here we just use npm to run the build
 cd /app
 echo building application...
+# sudo npm run build
 sudo npm install
-sudo npm run build
-cd /var/www/html
-sudo rm -rf *
-sudo cp -r /app/build/. /var/www/html
+
 # start the application with pm2
 echo starting application...
-#sudo pm2 start npm -- run production
+sudo pm2 start npm -- run production
